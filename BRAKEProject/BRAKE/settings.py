221e0ecfbabce887c3a2716a3802bdce16484309
= <BioPharmSoft@8e86bfb0e784>
2020-06-13 14:38:46 +0000

    remove old stuff

--- BRAKEProject/BRAKE/settings.py
+++ BRAKEProject/BRAKE/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^k0%#xw(+py%wpb#d*9n27x%)ebwy0(#%*5xntd_xm3714e_@f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['biopharmsoft.pythonanywhere.com']
 
 
 # Application definition
