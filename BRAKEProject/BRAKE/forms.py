8acde2334cfd7c7bd8f73dd0e075858ff2288692
Kyle Wathen <kylewathen@gmail.com>
2020-06-13 09:20:29 -0500

    Updates to add project and update project.   Also update to db to fix typo in name

--- BRAKEProject/BRAKE/forms.py
+++ BRAKEProject/BRAKE/forms.py
@@ -4,4 +4,5 @@ from brake_secure.models import RPack
 class RPackForm(ModelForm):
     class Meta:
         model = RPack
-        fields = ['name', 'description', 'autor', 'maintainer', 'keywords', 'cranLink', 'website']
+        fields = ['name', 'description', 'author', 'maintainer', 'keywords', 'cranLink', 'website', 'versionMajor',
+                  'versionMinor', 'versionPatch', 'versionBuild', 'approved','last_version_date']
